6c68b86911b244ba38bc2ceed65e16cd90afd449
Justin Philip <justin.philip@flightnetwork.com>
2020-08-27 23:52:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,16 @@ import './App.css';
 
 function App() {
   return (
+    // BEM naming convention
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Let Build it </h1>
+
+      {/* Header */}
+
+      {/* App Body*/}
+        {/* Sidebar*/}
+        {/* Feed */}
+        {/* Widgets */}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDkQ26nL-ufQdRJieskKeT3glXVnhKH-A0",
+    authDomain: "facebook-clone-893e3.firebaseapp.com",
+    databaseURL: "https://facebook-clone-893e3.firebaseio.com",
+    projectId: "facebook-clone-893e3",
+    storageBucket: "facebook-clone-893e3.appspot.com",
+    messagingSenderId: "473344173973",
+    appId: "1:473344173973:web:328ff216af7da5118ff89c",
+    measurementId: "G-S16TGD4P1C"
+  };
